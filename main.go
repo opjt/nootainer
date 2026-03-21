c9f00a78d5f3de09fe7df4e3d53c9218d96d246b
opjt <jtpark1957@gmail.com>
2026-03-21 09:07:23 +0900

    IPC,NET 격리 추가 및 hostname 설정

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 func run() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET,
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
@@ -53,6 +53,7 @@ func child() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	syscall.Sethostname([]byte("nootainer"))
 
 	if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
 		log.Fatal(err)
